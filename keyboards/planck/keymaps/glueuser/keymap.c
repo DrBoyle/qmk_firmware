e2783a000160816d96215bfa7d3457985e89907c
Stephen Boyle <dr.boyle@pm.me>
2020-02-23 21:18:07 -0800

    houdini macros

--- keyboards/planck/keymaps/glueuser/keymap.c
+++ keyboards/planck/keymaps/glueuser/keymap.c
@@ -3,9 +3,9 @@
 enum custom_keycodes {
   QMKBEST = SAFE_RANGE,
   EMAIL_A,
-	NAME_F,
-	NAME_L,
-	CITY,
+  NAME_F,
+  NAME_L,
+  CITY,
   GMAIL,
   HTTPS,
   EMAIL_B,
@@ -13,7 +13,11 @@ enum custom_keycodes {
   COPYALL,
   PASTEALL,
   P_QUOTE,
-  COMMA
+  COMMA,
+  //Houdini Macros
+  XBBOX,
+  YBBOX,
+  ZBBOX
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -100,6 +104,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 		break;
 
+    //Houdini Macros
+    case XBBOX:
+			if (record->event.pressed) {
+				SEND_STRING("bbox("SS_LSFT("'")SS_LSFT("'")", D_XSIZE)"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)"../");
+			} else {
+			}
+		break;
+
+    case YBBOX:
+			if (record->event.pressed) {
+				SEND_STRING("bbox("SS_LSFT("'")SS_LSFT("'")", D_YSIZE)"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)"../");
+			} else {
+			}
+		break;
+
+    case ZBBOX:
+			if (record->event.pressed) {
+				SEND_STRING("bbox("SS_LSFT("'")SS_LSFT("'")", D_ZSIZE)"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)"../");
+			} else {
+			}
+		break;
+
 	}
   return true;
 };
@@ -112,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_SFTENT,
-    MO(7),    KC_LGUI,  KC_LALT,  KC_LCTL,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
+    MO(8),    KC_LGUI,  KC_LALT,  KC_LCTL,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
 
   ),
 
@@ -122,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_SFTENT,
-    MO(7),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
+    MO(8),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
 
   ),
 
@@ -132,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,   KC_G,     KC_J,    KC_L,   KC_U,     KC_Y,     KC_SCLN,  KC_BSPC,
     KC_ESC,   KC_A,     KC_R,     KC_S,     KC_T,   KC_D,     KC_H,    KC_N,   KC_E,     KC_I,     KC_O,     KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_K,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_SFTENT,
-    MO(7),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
+    MO(8),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
 
   ),
 
@@ -142,11 +168,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,   KC_Y,     KC_F,    KC_G,   KC_C,     KC_R,     KC_L,   KC_BSPC,
     KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,   KC_I,     KC_D,    KC_H,   KC_T,     KC_N,     KC_S,   KC_SLSH,
     KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,   KC_X,     KC_B,    KC_M,   KC_W,     KC_V,     KC_Z,   KC_SFTENT,
-    MO(7),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT
+    MO(8),    KC_LCTL,  KC_LALT,  KC_LGUI,  MO(4),  KC_SPC,   KC_SPC,  MO(5),  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT
 
   ),
 
-  //Lower
+  //Lower //Symbols
   [4] = LAYOUT_ortho_4x12(
 
     KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_PIPE,
@@ -156,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  //Raise
+  //Raise  //Numbers
   [5] = LAYOUT_ortho_4x12(
 
     KC_GRV,   KC_BRID,  KC_BRIU,  KC_MRWD,  KC_MPLY,   KC_MFFD,   KC_MSTP,  KC_7,     KC_8,   KC_9,    KC_MINS,  KC_UNDS,
@@ -166,33 +192,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  //Both
+  //Both //Mouse
   [6] = LAYOUT_ortho_4x12(
 
-    OSL(8),   DM_RSTP,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_BTN2,  KC_BTN3,  KC_VOLU,  TG(6),
+    MO(7),    DM_RSTP,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_BTN2,  KC_BTN3,  KC_VOLU,  TG(6),
     KC_TRNS,  DM_REC1,  DM_PLY1,  KC_NO,    KC_NO,    KC_NO,    KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_VOLD,  KC_NO,
     KC_TRNS,  DM_REC2,  DM_PLY2,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_MUTE,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LGUI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
 
   ),
 
-  //Marcos
-  [7] = LAYOUT_ortho_4x12(
-
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
-
-  ),
-
   //System
-  [8] = LAYOUT_ortho_4x12(
+  [7] = LAYOUT_ortho_4x12(
 
     KC_NO,    DF(0),    DF(1),    DF(2),    DF(3),    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    RESET,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    AU_TOG,   CK_TOGG,   KC_NO,   KC_NO,    DEBUG,
     KC_NO,    MUV_DE,   MUV_IN,   MI_ON,    MI_OFF,   KC_NO,    KC_NO,    MU_TOG,   CK_DOWN,   CK_UP,   MU_MOD,   TERM_ON,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    TERM_OFF
 
+  ),
+
+  //Marcos
+  [8] = LAYOUT_ortho_4x12(
+
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    OSL(9),   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+
+  ),
+
+  //Marcos //Houdini
+  [9] = LAYOUT_ortho_4x12(
+
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    YBBOX,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    ZBBOX,    XBBOX,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+
   )
+
+  /*
+  //BLANCK
+  [0] = LAYOUT_ortho_4x12(
+
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+
+  )
+  */
 };
