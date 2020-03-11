a0e558efbaefb5d200953d418f992e1e81837b1b
Stephen Boyle <dr.boyle@pm.me>
2020-03-10 17:20:59 -0700

    ae macros

--- keyboards/planck/keymaps/glueuser/keymap.c
+++ keyboards/planck/keymaps/glueuser/keymap.c
@@ -14,10 +14,18 @@ enum custom_keycodes {
   PASTEALL,
   P_QUOTE,
   COMMA,
+  SGOOD,
+  THANK,
   //Houdini Macros
   XBBOX,
   YBBOX,
-  ZBBOX
+  ZBBOX,
+  //AE Macros
+  LOOPO,
+  LOOPP,
+  WIGGLE,
+  OSHOOT,
+  BOUNCE
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -104,6 +112,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 		break;
 
+    case SGOOD:
+			if (record->event.pressed) {
+				SEND_STRING("Sounds good. Thanks!"SS_TAP(X_ENT)SS_TAP(X_ENT)"-Stephen");
+			} else {
+			}
+		break;
+
+    case THANK:
+			if (record->event.pressed) {
+				SEND_STRING("Thanks!"SS_TAP(X_ENT)SS_TAP(X_ENT)"-Stephen");
+			} else {
+			}
+		break;
+
     //Houdini Macros
     case XBBOX:
 			if (record->event.pressed) {
@@ -122,6 +144,43 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ZBBOX:
 			if (record->event.pressed) {
 				SEND_STRING("bbox("SS_LSFT("'")SS_LSFT("'")", D_ZSIZE)"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)"../");
+			} else {
+			}
+		break;
+
+    //AE Macros
+
+    case LOOPO:
+			if (record->event.pressed) {
+				SEND_STRING("loopOut()"SS_TAP(X_LEFT));
+			} else {
+			}
+		break;
+
+    case LOOPP:
+			if (record->event.pressed) {
+				SEND_STRING("loopOut(type=)"SS_TAP(X_LEFT)SS_LSFT("'")SS_LSFT("'")SS_TAP(X_LEFT)"pingpong");
+			} else {
+			}
+		break;
+
+    case WIGGLE:
+			if (record->event.pressed) {
+				SEND_STRING("wiggle(9, 20)"SS_TAP(X_LEFT));
+			} else {
+			}
+		break;
+
+    case OSHOOT:
+			if (record->event.pressed) {
+				SEND_STRING("freq = 3;"SS_TAP(X_ENT)"decay = 8;"SS_TAP(X_ENT)"strength = 1;"SS_TAP(X_ENT)SS_TAP(X_ENT)"n = 0;"SS_TAP(X_ENT)"if (numKeys > 0){"SS_TAP(X_ENT)"n = nearestKey(time).index;"SS_TAP(X_ENT)"if (key(n).time > time) n--;"SS_TAP(X_ENT)"}"SS_TAP(X_ENT)"if (n > 0){"SS_TAP(X_ENT)"t = time - key(n).time;"SS_TAP(X_ENT)"amp = velocityAtTime(key(n).time - .001)*strength;"SS_TAP(X_ENT)"w = freq*Math.PI*2;"SS_TAP(X_ENT)"value + amp*(Math.sin(t*w)/Math.exp(decay*t)/w);"SS_TAP(X_ENT)"}else"SS_TAP(X_ENT)"value"SS_TAP(X_RGHT)SS_TAP(X_RGHT)SS_TAP(X_BSPC));
+			} else {
+			}
+		break;
+
+    case BOUNCE:
+			if (record->event.pressed) {
+				SEND_STRING("e = .7;"SS_TAP(X_ENT)"g = 5000;"SS_TAP(X_ENT)"nMax = 9;"SS_TAP(X_ENT)SS_TAP(X_ENT)"n = 0;"SS_TAP(X_ENT)"if (numKeys > 0){"SS_TAP(X_ENT)"n = nearestKey(time).index;"SS_TAP(X_ENT)"if (key(n).time > time) n--;"SS_TAP(X_ENT)"}"SS_TAP(X_ENT)"if (n > 0){"SS_TAP(X_ENT)"t = time - key(n).time;"SS_TAP(X_ENT)"v = -velocityAtTime(key(n).time - .001)*e;"SS_TAP(X_ENT)"vl = length(v);"SS_TAP(X_ENT)"if (value instanceof Array){"SS_TAP(X_ENT)"vu = (vl > 0) ? normalize(v) : [0,0,0];"SS_TAP(X_ENT)"}else{"SS_TAP(X_ENT)"vu = (v < 0) ? -1 : 1;"SS_TAP(X_ENT)"}"SS_TAP(X_ENT)SS_TAP(X_TAB)"tCur = 0;"SS_TAP(X_ENT)"segDur = 2*vl/g;"SS_TAP(X_ENT)"tNext = segDur;"SS_TAP(X_ENT)"nb = 1;"SS_TAP(X_ENT)"while (tNext < t && nb <= nMax){"SS_TAP(X_ENT)"vl *= e;"SS_TAP(X_ENT)"segDur *= e;"SS_TAP(X_ENT)"tCur = tNext;"SS_TAP(X_ENT)"tNext += segDur;"SS_TAP(X_ENT)"nb++"SS_TAP(X_ENT)"}"SS_TAP(X_ENT)"if(nb <= nMax){"SS_TAP(X_ENT)"delta = t - tCur;"SS_TAP(X_ENT)"value +  vu*delta*(vl - g*delta/2);"SS_TAP(X_ENT)"}else{"SS_TAP(X_ENT)"value"SS_TAP(X_ENT)"}"SS_TAP(X_ENT)"}else"SS_TAP(X_ENT)"value"SS_TAP(X_RGHT)SS_TAP(X_DOWN)SS_TAP(X_RGHT)SS_TAP(X_RGHT)SS_TAP(X_BSPC)SS_TAP(X_BSPC)SS_TAP(X_BSPC)SS_TAP(X_BSPC)SS_TAP(X_BSPC)SS_TAP(X_BSPC));
 			} else {
 			}
 		break;
@@ -212,22 +271,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  //Marcos
+  //Macros
   [8] = LAYOUT_ortho_4x12(
 
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    OSL(9),   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    THANK,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    OSL(10),  SGOOD,    KC_NO,    KC_NO,    KC_NO,    OSL(9),   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
 
   ),
 
-  //Marcos //Houdini
+  //Macros //Houdini
   [9] = LAYOUT_ortho_4x12(
 
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    YBBOX,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    ZBBOX,    XBBOX,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+
+  ),
+
+  //Macros //AE
+  [10] = LAYOUT_ortho_4x12(
+
+    KC_NO,    KC_NO,    WIGGLE,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    OSHOOT,   LOOPP,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    LOOPO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    BOUNCE,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
 
   )
